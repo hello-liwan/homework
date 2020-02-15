03d9032e4fb1ab13e5b60be8a0fe60dc32877892
liwanwan6 <58982538+liwanwan6@users.noreply.github.com>
2020-02-15 14:17:20 +0800

    Add files via upload

---
+++ sort.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+struct score {
+	char name[20];
+	double chinese, english;
+	bool operator<(const score& a)
+		const {
+		return chinese>a.chinese;
+	}
+};
+struct score stu[3];
+int main()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> stu[i].name >> stu[i].chinese >> stu[i].english;
+	}
+	sort(stu, stu + 3);
+	for (int i = 0; i < 3; i++)
+	{
+		cout<< stu[i].name << stu[i].chinese << stu[i].english << endl;
+	}
+	return 0;
+}
